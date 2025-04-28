43f444e5025ba8d0898af8d99aaaa9bb884579ab
A-lineed <aline.edvania.franca@outlook.com>
2025-04-28 19:49:23 -0300

    Mockando retorno da API para teste shows the right data for all rendered stories

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -83,12 +83,28 @@ describe('Hacker Stories', () => {
       })
 
       context('List of stories', () => {
-        // Since the API is external,
-        // I can't control what it will provide to the frontend,
-        // and so, how can I assert on the data?
-        // This is why this test is being skipped.
-        // TODO: Find a way to test it out.
-        it.skip('shows the right data for all rendered stories', () => { })
+        it.only('shows the right data for all rendered stories', () => {
+          const stories = require('../fixtures/stories')
+
+          cy.get('.item')
+            .first()
+            .should('contain', stories.hits[0].title)
+            .and('contain', stories.hits[0].author)
+            .and('contain', stories.hits[0].num_comments)
+            .and('contain', stories.hits[0].points)
+          cy.get(`.item a:contains(${stories.hits[0].title})`)
+            .should('have.attr', 'href', stories.hits[0].url)
+
+          cy.get('.item')
+            .last()
+            .should('contain', stories.hits[1].title)
+            .and('contain', stories.hits[1].author)
+            .and('contain', stories.hits[1].num_comments)
+            .and('contain', stories.hits[1].points)
+          cy.get(`.item a:contains(${stories.hits[1].title})`)
+            .should('have.attr', 'href', stories.hits[1].url)
+
+        })
 
         it('shows one less story after dimissing the first one', () => {
           cy.get('.button-small')
@@ -116,83 +132,83 @@ describe('Hacker Stories', () => {
     })
 
     context
-    ('Search', () => {
-      beforeEach(() => {
-        cy.intercept(
-          'GET',
-          `**/search?query=${initialTerm}&page=0`,
-          { fixture: 'empty' }
-        ).as('getEmptyStories')
-
-        cy.intercept(
-          'GET',
-          `**/search?query=${newTerm}&page=0`,
-          { fixture: 'stories' }
-        ).as('getStories')
-
-        cy.visit('/')
-        cy.wait('@getEmptyStories')
-
-        cy.get('#search')
-          .clear()
-      })
-
-      it('types and hits ENTER', () => {
-        cy.get('#search')
-          .type(`${newTerm}{enter}`)
-
-        cy.wait('@getStories')
-
-        cy.get('.item').should('have.length', 2)
-        cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
-      })
-
-      it('types and clicks the submit button', () => {
-        cy.get('#search')
-          .type(newTerm)
-        cy.contains('Submit')
-          .click()
-
-        cy.wait('@getStories')
-
-        cy.get('.item').should('have.length', 2)
-        cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
-      })
-
-      it('types and submits the form directly', () => {
-        cy.get('#search').type(newTerm)
-
-        cy.get('form').submit()
-
-        cy.wait('@getNewTermStories')
-
-        cy.get('.item').should('have.length', 20)
-      })
-
-      context('Last searches', () => {
-        it.only('shows a max of 5 buttons for the last searched terms', () => {
-          const faker = require('faker')
+      ('Search', () => {
+        beforeEach(() => {
+          cy.intercept(
+            'GET',
+            `**/search?query=${initialTerm}&page=0`,
+            { fixture: 'empty' }
+          ).as('getEmptyStories')
 
           cy.intercept(
             'GET',
-            '**/search**',
-            { fixture: 'empty'}
-          ).as('getRandomStories')
+            `**/search?query=${newTerm}&page=0`,
+            { fixture: 'stories' }
+          ).as('getStories')
 
-          Cypress._.times(6, () => {
-            cy.get('#search')
-              .clear()
-              .type(`${faker.random.word()}{enter}`)
-            cy.wait('@getRandomStories')
+          cy.visit('/')
+          cy.wait('@getEmptyStories')
+
+          cy.get('#search')
+            .clear()
+        })
+
+        it('types and hits ENTER', () => {
+          cy.get('#search')
+            .type(`${newTerm}{enter}`)
+
+          cy.wait('@getStories')
+
+          cy.get('.item').should('have.length', 2)
+          cy.get(`button:contains(${initialTerm})`)
+            .should('be.visible')
+        })
+
+        it('types and clicks the submit button', () => {
+          cy.get('#search')
+            .type(newTerm)
+          cy.contains('Submit')
+            .click()
+
+          cy.wait('@getStories')
+
+          cy.get('.item').should('have.length', 2)
+          cy.get(`button:contains(${initialTerm})`)
+            .should('be.visible')
+        })
+
+        it('types and submits the form directly', () => {
+          cy.get('#search').type(newTerm)
+
+          cy.get('form').submit()
+
+          cy.wait('@getNewTermStories')
+
+          cy.get('.item').should('have.length', 20)
+        })
+
+        context('Last searches', () => {
+          it('shows a max of 5 buttons for the last searched terms', () => {
+            const faker = require('faker')
+
+            cy.intercept(
+              'GET',
+              '**/search**',
+              { fixture: 'empty' }
+            ).as('getRandomStories')
+
+            Cypress._.times(6, () => {
+              cy.get('#search')
+                .clear()
+                .type(`${faker.random.word()}{enter}`)
+              cy.wait('@getRandomStories')
+            })
+
+            cy.get('.last-searches button')
+              .should('have.length', 5)
           })
-
-          cy.get('.last-searches button')
-            .should('have.length', 5)
         })
       })
-    })
   })
 
   context('Errors', () => {
